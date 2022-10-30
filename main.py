6f7fdcd3217c64880bd1a03e2c2259d9f7a38369
kostayli <99755309+kostayli@users.noreply.github.com>
2022-10-30 22:42:00 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from nltk import load_parser
+cp = load_parser('sql0.fcfg')
+query = 'What cities are located in China'
+trees = list(cp.parse(query.split()))
+answer = trees[0].label()['SEM']
+answer = [s for s in answer if s]
+q = ' '.join(answer)
+print(q)
